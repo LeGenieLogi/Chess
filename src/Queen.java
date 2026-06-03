213de8dce9728100dfdde0680f4011c89d84f74d
LGL925 <kouakoujeanelisee592@gmail.com>
2026-06-03 11:24:20 +0000

    Ajout des fonctionnalités de le reine

--- src/Queen.java
+++ src/Queen.java
@@ -1,11 +1,23 @@
 public class Queen extends Piece {
+
+    // Vérifie si le déplacement de la reine est valide
     boolean isValidMove(Position newPosition) {
+
         if (!super.isValidMove(newPosition)) {
             return false;
         }
 
-        boolean isVerticalOrHorizontal = newPosition.column == this.column || newPosition.row == this.row;
-        boolean isDiagonal = Math.abs(newPosition.column - this.column) == Math.abs(newPosition.row - this.row);
+        // Déplacement vertical ou horizontal
+        boolean isVerticalOrHorizontal =
+                newPosition.column == this.column ||
+                        newPosition.row == this.row;
+
+        // Déplacement en diagonale
+        boolean isDiagonal =
+                Math.abs(newPosition.column - this.column) ==
+                        Math.abs(newPosition.row - this.row);
+
+        // La reine peut se déplacer en ligne droite ou en diagonale
         return isVerticalOrHorizontal || isDiagonal;
     }
 }
