5e52cc87d8702297b72c72a87c2fc5e903fcc878
LGL925 <kouakoujeanelisee592@gmail.com>
2026-06-03 11:25:13 +0000

    Ajout des fonctionnalités main pour le test

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Queen queen = new Queen();
+        Position testPosition = new Position(3, 3);
+
+        if (queen.isValidMove(testPosition)) {
+            System.out.println("Yes, I can move there.");
+        } else {
+            System.out.println("Nope, can't do!");
+        }
+    }
+}
